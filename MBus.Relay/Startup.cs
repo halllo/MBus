685b8131172f45487a11a14972b8da63d31c0189
Manuel Naujoks <mnaujoks@gmail.com>
2016-04-04 19:46:23 +0200

    hidden emit container, emit api

--- MBus.Relay/Startup.cs
+++ MBus.Relay/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.Owin;
 using Microsoft.Owin.Cors;
 using Owin;
 using System.Threading.Tasks;
+using System.Web.Http;
 
 [assembly: OwinStartup(typeof(MBus.Relay.Startup))]
 
@@ -14,6 +15,36 @@ namespace MBus.Relay
 		{
 			app.UseCors(CorsOptions.AllowAll);
 			app.MapSignalR();
+			app.UseWebApi(ConfigureWebApi());
+		}
+
+		private HttpConfiguration ConfigureWebApi()
+		{
+			var config = new HttpConfiguration();
+
+			config.Routes.MapHttpRoute("DefaultApi",
+				"api/{controller}/{id}",
+				new { id = RouteParameter.Optional });
+
+			return config;
+		}
+	}
+
+	public class MBusController : ApiController
+	{
+		// POST /api/mbus
+		public void Post([FromBody]Message message)
+		{
+			if (message != null && !string.IsNullOrWhiteSpace(message.Sender) && !string.IsNullOrWhiteSpace(message.Content))
+			{
+				var myHub = GlobalHost.ConnectionManager.GetHubContext<MyHub>();
+				myHub.Clients.All.addMessage(message.Sender, message.Content);
+			}
+		}
+		public class Message
+		{
+			public string Sender { get; set; }
+			public string Content { get; set; }
 		}
 	}
 
