3a0b9fe6b7e7473b7a184f899e0230943e8f07de
Manuel Naujoks <mnaujoks@gmail.com>
2016-10-28 15:10:46 +0200

    Relay-Server remembers last 50 messages

--- MBus.Relay/Startup.cs
+++ MBus.Relay/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.Owin.Cors;
 using Owin;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Collections.Generic;
+using System.Linq;
 
 [assembly: OwinStartup(typeof(MBus.Relay.Startup))]
 
@@ -30,6 +32,19 @@ namespace MBus.Relay
 		}
 	}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 	public class MBusController : ApiController
 	{
 		// POST /api/mbus
@@ -46,12 +61,23 @@ namespace MBus.Relay
 			public string Sender { get; set; }
 			public string Content { get; set; }
 		}
+
+		// GET /api/mbus
+		public IEnumerable<RecentMessages.RecentMessage> Get()
+		{
+			return RecentMessages.Get;
+		}
 	}
+
+
+
+
 
 	public class MyHub : Hub
 	{
 		public void Send(string userName, string message)
 		{
+			RecentMessages.Record(userName, message);
 			Clients.All.addMessage(userName, message);
 		}
 		public override Task OnConnected()
@@ -65,6 +91,34 @@ namespace MBus.Relay
 		public override Task OnReconnected()
 		{
 			return base.OnReconnected();
+		}
+	}
+
+
+
+
+
+	public static class RecentMessages
+	{
+		public class RecentMessage
+		{
+			public ulong Id { get; set; }
+			public string UserName { get; set; }
+			public string Message { get; set; }
+		}
+
+		static ulong messageId = 0;
+		static List<RecentMessage> recorded = new List<RecentMessage>();
+
+		public static IEnumerable<RecentMessage> Get { get { lock(recorded) return recorded.ToArray(); } }
+
+		public static void Record(string userName, string message)
+		{
+			lock (recorded)
+			{
+				recorded.Insert(0, new RecentMessage { Id = messageId++, UserName = userName, Message = message });
+				if (recorded.Count > 50) recorded.RemoveAt(recorded.Count - 1);
+			}
 		}
 	}
 }
